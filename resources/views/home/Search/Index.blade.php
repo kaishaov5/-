24691f6cc1a464d4824b63b5a36036908ebf668b
HallelujahR <15043319951@163.com>
2018-10-12 14:50:54 +0800

    three day raven

--- resources/views/home/Search/Index.blade.php
+++ resources/views/home/Search/Index.blade.php
@@ -17,167 +17,41 @@
 	</div>
 
 	<div id="s-detail">
-		<!-- 一侧可以容纳19条帖子 -->
-		<div class="s-detail-left">
-			<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
+		<!-- 一侧可以容纳条帖子 -->
+		@if($post->count() > 0)
+			<div class="s-detail">
+				@foreach($post as $k=>$v)
+				<div class="s-detail-line">
+					<div class="s-post-img">
+						<a href="">
+							<img src="/home/images/folder.gif">
+						</a>
+					</div>
+					<div class="s-post-title">
+						<a href="/bbs/post/{{$v->id}}.html">{{$v->title}}</a>
+					</div>
+					<div class="s-detail-d">
+						<span>
+							回复 : {{$v->replies->count()}}
+						</span>
+						<span>
+							热度 : {{$v->access_count}}
+						</span>
+					</div>
+					<div class="s-time">
+						{{$v->created_at}}
+					</div>
+					<div class="s-post-auth">
+						<a href="/personal/{{$v->user->id}}">作者 ： {{$v->user->name}}</a>
+					</div>
+				</div>	
+				@endforeach
 			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
+		@else
+			<div class="s-nopost">
+				暂无相关帖子~
 			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-						<div class="s-detail-left-line">
-				<div class="s-post-title">
-					<a href="">帖子的名称帖子的名称帖子的名称帖子的名称</a>
-				</div>
-				<div class="s-post-auth">
-					<a href="">作者：XXXX</a>
-				</div>
-			</div>
-
-		</div>
-
-		<div class="s-detail-right">
-			
-		</div>
-
+		@endif
 	</div>
 
 	<!-- 文章部分 -->
@@ -186,11 +60,76 @@
 		<span class="s-head-b"><a href="">更多</a></span>
 	</div>
 
+	<div id="s-detail">
+		<!-- 一侧可以容纳条帖子 -->
+		@if($article->count() > 0)
+			<div class="s-detail">
+				@foreach($article as $k=>$v)
+				<div class="s-detail-line">
+					<div class="s-post-img" >
+						<a href="/cate/{{$v->cate->id}}.html" style="color:red"> 
+							{{$v->cate->cate}}
+						</a>
+					</div>
+					<div class="s-post-title">
+						<a href="/article/{{$v->id}}.html">{{$v->title}}</a>
+					</div>
+					<div class="s-detail-d">
+						<span>
+							评论数量 : {{$v->comments->count()}}
+						</span>
+						<span>
+							阅读量 : {{$v->access_count}}
+						</span>
+					</div>
+					<div class="s-time">
+						{{$v->created_at}}
+					</div>
+					<div class="s-post-auth">
+						<a href="/personal/{{$v->user->id}}">作者 ： {{$v->user->name}}</a>
+					</div>
+				</div>	
+				@endforeach
+			</div>
+		@else
+			<div class="s-nopost">
+				暂无相关文章~
+			</div>
+		@endif
+	</div>
+
 
 	<!-- 用户部分 -->
 	<div class="s-head">
 		<span class="s-head-a">用户</span>
 		<span class="s-head-b"><a href="">更多</a></span>
+	</div>
+
+
+	<div class="s-detail">
+		@if($user -> count() > 0)
+			@foreach($user as $k => $v)
+			<div class="s-user">
+				<div class="s-user-img">
+					<a href="/personal/{{$v->id}}">
+						<img src="/home/images/login.jpg">
+					</a>
+				</div>
+				<div class="s-user-detail">
+					<div class="s-user-name">
+						<a href="/personal/{{$v->id}}">{{$v->name}}</a>
+					</div>
+					<div>
+						个人主页
+					</div>
+				</div>
+			</div>
+			@endforeach
+		@else
+		<div class="s-nopost">
+			暂无相关用户~
+		</div>
+		@endif
 	</div>
 
 </div>
